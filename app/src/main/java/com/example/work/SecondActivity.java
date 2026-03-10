6aa192a29a58a4ff3c49df2d0dea2bbfc8f29766
dotaror <boosgames2004@gmail.com>
2026-03-10 12:16:13 +0300

    Two Activities

--- app/src/main/java/com/example/work/SecondActivity.java
+++ app/src/main/java/com/example/work/SecondActivity.java
@@ -6,18 +6,18 @@ import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
 
-        Button buttonToPage2 = findViewById(R.id.buttonToPage2);
-        buttonToPage2.setOnClickListener(new View.OnClickListener() {
+        Button buttonToPage1 = findViewById(R.id.buttonToPage1);
+        buttonToPage1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
                 startActivity(intent);
             }
         });
